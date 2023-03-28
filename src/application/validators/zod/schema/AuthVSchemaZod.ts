294b1575914283031eeee718cc67cbb0cf5974da
thenewsatria <satriabelajarterus@gmail.com>
2023-03-28 13:39:32 +0700

    fix minor validator schema

--- src/application/validators/zod/schema/AuthVSchemaZod.ts
+++ src/application/validators/zod/schema/AuthVSchemaZod.ts
@@ -1,4 +1,4 @@
-import {z, ZodSchema} from "zod";
+import {z, ZodSchema, ZodPipeline, ZodString, ZodEffects} from "zod";
 import LoginRequestDTO from "../../../../interfaces/dtos/auth/LoginRequestDTO";
 import RegisterRequestDTO from "../../../../interfaces/dtos/auth/RegisterRequestDTO";
 import AuthVSchema from "../../../../interfaces/validators/schemas/AuthVSchema";
@@ -13,10 +13,24 @@ class AuthVSchemasZod implements AuthVSchema {
 
         return AuthVSchemasZod.instance
     }
+
+    private setNoEmptyString(attributeName: string,
+        handler: ZodString): ZodPipeline<ZodEffects<ZodString, string | undefined, string>, ZodString> {
+        return z.string({
+                required_error: `${attributeName} is required`,
+                invalid_type_error: `${attributeName} must be a string`
+            })
+            .transform((str) => str === "" ? undefined : str)
+            .pipe(
+                handler
+            )
+    }
     
     getRegisterRequestSchema(): ZodSchema<RegisterRequestDTO> {
         return z.object({
-            username: z.string({
+            username: this.setNoEmptyString(
+                "Username",
+                z.string({
                     required_error: "Username is required",
                     invalid_type_error: "Username must be a string"
                 })
@@ -29,18 +43,34 @@ class AuthVSchemasZod implements AuthVSchema {
                 .regex(/^[a-z0-9_.]+$/, {
                     message: "Username can only contain lowercase characters, period, and underscores"
                 }),
-            email: z.string({
+            ),
+            email: this.setNoEmptyString(
+                "Email",
+                z.string({
                     required_error: "Email is required",
                     invalid_type_error: "Email must be a string"
                 })
                 .email({
                     message: "Invalid email"
                 }),
-            password: z.string()
+            ),
+            password: this.setNoEmptyString(
+                "Password",
+                z.string({
+                    required_error: "Password is required",
+                    invalid_type_error: "Password must be a string"
+                })
                 .min(8, {
                     message: "Password must be at least 8 characters long"
-                }),
-            confirmPassword: z.string(),
+                })
+            ),
+            confirmPassword: this.setNoEmptyString(
+                "Confirm Password",
+                z.string({
+                    required_error: "Confirm Password is required",
+                    invalid_type_error: "Confirm Password must be a string"
+                })
+            ),
             IP: z.string().ip(),
             userAgent: z.string()
         })
@@ -58,14 +88,20 @@ class AuthVSchemasZod implements AuthVSchema {
 
     getLoginRequestSchema(): ZodSchema<LoginRequestDTO> {
         return z.object({
-            credential: z.string({
-                required_error: "Email or username is required",
-                invalid_type_error: "Email or username must be a string"
-            }),
-            password: z.string({
-                required_error: "Password is required",
-                invalid_type_error: "Password must be a string"
-            }),
+            credential: this.setNoEmptyString(
+                "Email or Username",
+                z.string({
+                    required_error: "Email or username is required",
+                    invalid_type_error: "Email or username must be a string"
+                })
+            ),
+            password: this.setNoEmptyString(
+                "Password",
+                z.string({
+                    required_error: "Password is required",
+                    invalid_type_error: "Password must be a string"
+                })
+            ),
             IP: z.string().ip(),
             userAgent: z.string()
         })
